0c12b9990f108f048b41338708501353251e9619
Farnabaz <farnabaz@gmail.com>
2023-05-29 14:34:18 +0200

    test

--- app.config.ts
+++ app.config.ts
@@ -24,10 +24,11 @@ export default defineAppConfig({
       fluid: true
     },
     header: {
-      logo: true,
+      logo: false,
       showLinkIcon: true,
       exclude: [],
-      fluid: true
+      fluid: true,
+      title: 'Test'
     },
     footer: {
       iconLinks: [
